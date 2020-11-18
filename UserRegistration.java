abb0cdb85db3af186943df083c7d2437900a168d
naveeny061 <naveeny061@gmail.com>
2020-11-18 18:47:37 +0530

    Added: Method to enter valid Password minimum 8 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -22,11 +22,17 @@ public class UserRegistration {
 		Pattern pattern = Pattern.compile(expression);
 		return pattern.matcher(email).matches();
 	}
-	
+
 	public boolean validateMobile(String mobile) {
 		String expression = "^[+]?[0-9]{2}[ ][0-9]{10}$";
 		Pattern pattern = Pattern.compile(expression);
 		return pattern.matcher(mobile).matches();
+	}
+
+	public boolean validatePassword(String password) {
+		String expression = "^.{8,}$";
+		Pattern pattern = Pattern.compile(expression);
+		return pattern.matcher(password).matches();
 	}
 
 	public static void main(String[] args) {
@@ -68,7 +74,7 @@ public class UserRegistration {
 				System.out.println("Please enter valid email address");
 			}
 		}
-		
+
 		while (true) {
 			System.out.println("Enter Mobile Number");
 			String mobile = scanner.nextLine();
@@ -78,6 +84,18 @@ public class UserRegistration {
 				break;
 			} else {
 				System.out.println("Please enter valid mobile number");
+			}
+		}
+
+		while (true) {
+			System.out.println("Enter Password");
+			String password = scanner.nextLine();
+			boolean check = user.validatePassword(password);
+			if (check) {
+				System.out.println(user.validatePassword(password));
+				break;
+			} else {
+				System.out.println("Please enter valid password");
 			}
 		}
 	}
