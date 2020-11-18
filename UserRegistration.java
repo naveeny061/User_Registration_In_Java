2ac56e3ab8f82e558315e18652f663937e02718f
naveeny061 <naveeny061@gmail.com>
2020-11-18 17:36:22 +0530

    Added: Method to enter valid first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,10 +1,31 @@
 package pattern;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration System ");
+	public boolean validateFirstName(String firstname) {
+		String expression = "^[A-Z][a-z]{2,}$";
+		Pattern pattern = Pattern.compile(expression);
+		return pattern.matcher(firstname).matches();
+	}
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		UserRegistration user = new UserRegistration();
+		System.out.println("Welcome to User Registration System ");
+		while (true) {
+			System.out.println("Enter First Name");
+			String firstName = scanner.nextLine();
+			boolean check = user.validateFirstName(firstName);
+			if (check) {
+				System.out.println(user.validateFirstName(firstName));
+				break;
+			} else {
+				System.out.println("First name starts with Cap and has minimum 3 characters");
+			}
+		}
 	}
 
 }
