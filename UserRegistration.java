eb1d6090473f26d03be4b7b1bc1b3af6ce89fb3c
naveeny061 <naveeny061@gmail.com>
2020-11-18 17:43:38 +0530

    Added: Method to enter valid last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,6 +10,12 @@ public class UserRegistration {
 		Pattern pattern = Pattern.compile(expression);
 		return pattern.matcher(firstname).matches();
 	}
+	
+	public boolean validateLastName(String lastname) {
+		String expression = "^[A-Z][a-z]{2,}$";
+		Pattern pattern = Pattern.compile(expression);
+		return pattern.matcher(lastname).matches();
+	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -24,6 +30,17 @@ public class UserRegistration {
 				break;
 			} else {
 				System.out.println("First name starts with Cap and has minimum 3 characters");
+			}
+		}
+		while (true) {
+			System.out.println("Enter Last Name");
+			String lastName = scanner.nextLine();
+			boolean check = user.validateFirstName(lastName);
+			if (check) {
+				System.out.println(user.validateFirstName(lastName));
+				break;
+			} else {
+				System.out.println("Last name starts with Cap and has minimum 3 characters");
 			}
 		}
 	}
